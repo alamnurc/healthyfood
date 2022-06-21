93268cd4e6348a860a9166d575ca820957916a75
alamnurcahaya <lammnurc@outlook.com>
2022-06-21 09:18:59 +0700

    test

---
+++ script.js
@@ -0,0 +1,12 @@
+//Tutorial Advance Form//
+function submit_form(){  
+    alert("Berhasil Terkirim");  
+    }
+
+//Tutorial Advance redirect//
+function page_redirect() {
+    location.replace("https://www.zahranrafif.com")
+    }
+
+
+
